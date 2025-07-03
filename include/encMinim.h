24ae2886fe107a0aa04cd0b3f323db83b9c52a56
beginend <lottocad@gmail.com>
2025-07-03 08:39:55 +0200

    After Help Me from PlatformIO Community Forum

--- include/encMinim.h
+++ include/encMinim.h
@@ -9,6 +9,7 @@
 
 
 #define ENCMINIM_H
+#include <Arduino.h>
 
 #pragma once
 #define _ME_ENC_FAST 30
@@ -118,55 +119,4 @@ void encMinim::tick(bool hold) {
   }
 }
 
-byte encMinim::getState() {
-  return _encState;
-}
-void encMinim::rst() {
-  _encState = 0;
-}
-boolean encMinim::isFast() {
-  if (_fast) return true;
-  else return false;
-}
-boolean encMinim::isTurn() {
-  if (_encState > 0 && _encState < 5) {
-    return true;
-  } else return false;
-}
-boolean encMinim::isRight() {
-  if (_encState == 1) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
-boolean encMinim::isLeft() {
-  if (_encState == 2) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
-boolean encMinim::isRightH() {
-  if (_encState == 3) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
-boolean encMinim::isLeftH() {
-  if (_encState == 4) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
-boolean encMinim::isClick() {
-  if (_encState == 5) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
-boolean encMinim::isHolded() {
-  if (_encState == 6) {
-    _encState = 0;
-    return true;
-  } else return false;
-}
 #endif // ENCMINIM_H_
